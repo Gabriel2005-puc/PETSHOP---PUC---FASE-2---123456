22f54720d55ddecdaebaf620bd4dbf61c6140fcf
Gabriel2005-puc <gabrieldecarvalhosilva15@gmail.com>
2024-09-08 17:31:24 -0300

    Initial commit

---
+++ JS - PETSHOP.js
@@ -0,0 +1,10 @@
+// STATUS DE ENTREGA
+document.addEventListener('DOMContentLoaded', function() {
+    const statusEntrega = document.getElementById('statusEntrega');
+    statusEntrega.innerHTML = "<p>Seu pedido está a caminho!</p>";
+});
+
+// CANCELAMENTO
+function cancelarAtividade() {
+    alert("Atividade cancelada com sucesso.");
+}
